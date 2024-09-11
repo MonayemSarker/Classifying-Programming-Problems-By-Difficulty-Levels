b82db23f850061d635142a234d6005ee77713231
Sampad Sikder <60497178+SampadSikder@users.noreply.github.com>
2024-09-11 13:31:12 +0600

    problem and problem set schema added

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
+import { ProblemsModule } from './problems/problems.module';
 
 @Module({
   imports: [UsersModule, ConfigModule.forRoot({
     isGlobal: true,
-  }),],
+  }), ProblemsModule,],
   controllers: [AppController],
   providers: [AppService],
 })
